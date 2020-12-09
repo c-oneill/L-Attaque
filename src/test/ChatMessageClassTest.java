5875f1d1b929c2dc1e273e733268bbc89c8a9ade
Ahmed <jszs2009@email.arizona.edu>
2020-12-09 11:49:29 -0700

    ChatMessage JUnit test

---
+++ src/test/ChatMessageClassTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import stratego.ChatMessage;
+import stratego.Piece;
+
+public class ChatMessageClassTest {
+	@Test
+	public void constructorTest() {
+		ChatMessage chatMessage = new ChatMessage("Hello", Piece.RED);
+		assertTrue(chatMessage instanceof ChatMessage);
+	}
+	@Test
+	public void getMessageTest() {
+		ChatMessage chatMessage = new ChatMessage("Hello", Piece.RED);
+		assertEquals(chatMessage.getMessage(), "Hello");
+	}
+	@Test
+	public void getColorTest() {
+		ChatMessage chatMessage = new ChatMessage("Hello", Piece.RED);
+		assertEquals(chatMessage.getColor(), Piece.RED);
+	}
+}
