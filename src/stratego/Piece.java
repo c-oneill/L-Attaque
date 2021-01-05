1f904c74d8354b1d2bc2c32e82328677be4b090d
Caroline ONeill <59894440+c-oneill@users.noreply.github.com>
2021-01-05 12:02:01 -0700

    working version of maven build

--- src/stratego/Piece.java
+++ src/stratego/Piece.java
@@ -22,7 +22,7 @@ import java.io.Serializable;
  */
 public class Piece implements Serializable
 {
-
+	
 	private static final long serialVersionUID = 1L;
 	
 	public static final int NONE = 0; // neither player
